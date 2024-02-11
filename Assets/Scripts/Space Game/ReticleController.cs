82ad1a15125ac424a60d947eb8d0ed893aaf7159
lstevensN <lstevens@student.neumont.edu>
2024-02-11 12:12:34 -0700

    Rail Scene added

--- Assets/Scripts/Space Game/ReticleController.cs
+++ Assets/Scripts/Space Game/ReticleController.cs
@@ -1,14 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
 using UnityEngine;
 
-public class KinematicController : MonoBehaviour
+public class ReticleController : MonoBehaviour
 {
     [SerializeField, Range(0, 40)] float speed = 1;
     [SerializeField] float maxDistance = 5;
-
-    // public float health = 100;
 
     // Update is called once per frame
     void Update()
@@ -22,12 +19,5 @@ public class KinematicController : MonoBehaviour
         transform.localPosition += force;
 
         transform.localPosition = Vector3.ClampMagnitude(transform.localPosition, maxDistance);
-        // transform.localRotation = new Quaternion(-direction.y * 0.2f, direction.x * 0.2f, 0, 1);
-
-        Quaternion qyaw = Quaternion.AngleAxis(direction.x * 20, Vector3.up);
-        Quaternion qpitch = Quaternion.AngleAxis(-direction.y * 20, Vector3.right);
-
-        Quaternion rotation = qyaw * qpitch;
-        transform.localRotation = Quaternion.Slerp(transform.localRotation, rotation, Time.deltaTime * 10);
     }
 }
